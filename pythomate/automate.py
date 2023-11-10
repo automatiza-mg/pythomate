35412585094a387476c6234c78b60bedbb169c6d
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-11-10 10:12:51 -0300

    Retira caminho completo instalação automate
    Necessário cadastrar caminho completo path windows
    See #8

--- pythomate/automate.py
+++ pythomate/automate.py
@@ -1,23 +1,23 @@
 from pywinauto.application import Application
 
 def run_flow(flow):
-	power_automate_exe_path = 'C:/Program Files (x86)/Power Automate Desktop/PAD.Console.Host.exe'
+	power_automate_exe_path = 'PAD.Console.Host.exe'
 	app = Application(backend="uia").start(power_automate_exe_path).connect(
 																			title='Power Automate',
 																			timeout=100,
 																			)
 	dlg_spec = app.PowerAutomate
-	
+
 	# Clica linha do fluxo
 	flow_line = dlg_spec.child_window(
-									  title=flow, 
+									  title=flow,
 									  control_type='DataItem',
 									  ).wrapper_object()
 	flow_line.click_input()
-	# Clica na execução do fluxo	
+	# Clica na execução do fluxo
 	flow_button = dlg_spec.child_window(
-										title="Executar", 
-										auto_id="StartFlowButton", 
+										title="Executar",
+										auto_id="StartFlowButton",
 										control_type="Button"
 										).wrapper_object()
 	flow_button.click_input()
@@ -28,7 +28,7 @@ def run_flow(flow):
 									  auto_id="Button",
 									  control_type="Button"
 									  )
-	
+
 	ok_button.click_input()
 
 if __name__ == '__main__':
